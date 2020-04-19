852ad9bcb35a61c7b4afc952487b41fecf985a84
Vitoria <vitoriagoncalvesgaspar@hotmail.com>
2020-04-19 16:55:41 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.png";
+import car from "./assets/images/car.png";
+import motorcycle from "./assets/images/motorcycle.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="imgLogo" alt="logo" />
       </header>
+
+      <div className="nav">
+        <div className="buttonsNav">
+          <div className="buttonNavCar">
+            <div className="areaIcon">
+              <img src={car} className="icon" alt="logo" />
+            </div>
+            <div className="textButtonNav">
+              <h3>COMPRAR</h3>
+              <h1>CARROS</h1>
+            </div>
+          </div>
+          <div className="buttonNavMotorcycle">
+            <div className="areaIcon">
+              <img src={motorcycle} className="icon" alt="logo" />
+            </div>
+            <div className="textButtonNav">
+              <h3>COMPRAR</h3>
+              <h1>CARROS</h1>
+            </div>
+          </div>
+        </div>
+        <input></input>
+      </div>
     </div>
   );
 }
